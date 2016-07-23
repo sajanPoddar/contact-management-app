7fa9e0c45a315d189f568a83dc33463193c23513
sajan <sajanpoddar98@gmail.com>
2016-07-23 23:35:31 +0600

    admin lte view integrated

--- resources/views/dashboard/partials/script.blade.php
+++ resources/views/dashboard/partials/script.blade.php
@@ -5,9 +5,9 @@
 <!-- Bootstrap 3.3.6 -->
 <script src="{{ asset('components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 <!-- SlimScroll -->
-<script src="../../components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="{{ asset('components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
 <!-- FastClick -->
-<script src="../../components/AdminLTE/plugins/fastclick/fastclick.js"></script>
+<script src="{{ asset('components/AdminLTE/plugins/fastclick/fastclick.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('components/AdminLTE/dist/js/app.min.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
